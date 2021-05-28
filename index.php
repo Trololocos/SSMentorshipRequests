22d0906ad3712a4298f07012a84b55cf6c81ef83
Trololocos <47633543+Trololocos@users.noreply.github.com>
2021-05-28 11:19:08 +0200

    Added the sql handling logic

--- index.php
+++ index.php
@@ -26,22 +26,43 @@
         </tr>
         
         <?php
-    function add_row($row_array){ //st
+    function add_row($row_array){ 
+        echo "<tr>";
+        //Add cells
         foreach($row_array as $cell)
         {
-        //Add cell
         echo "<td>".$cell."</td>";
         }
+        
+        echo "</tr>";
     }
-    //They should be outside of the source code in a config file for later implementation
-    $DBpass = "root";
-    $DBuser = "root";
-    $DBname = "SSMR";
-    $conn = new mysqli("localhost:3306", $DBuser, $DBpass, $DBname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
+    function connect(){
+        //They should be outside of the source code in a config file for later implementation
+        $DBpass = "root";
+        $DBuser = "root";
+        $DBname = "SSMR";
+        $conn = new mysqli("localhost:3306", $DBuser, $DBpass, $DBname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }  
+        return $conn;
+    }
+    function fill_table($connection){
+        //puts all the requests into the table
+        $result =  $connection->query("SELECT * FROM requests");
+        while(true)
+        {
+            $i = $result->fetch_row();
+            if(!$i) break;
+            add_row($i);
+        }
+    }
+
+    fill_table(connect());
+        
+
     
+
     ?> 
 </table>
 </body>
